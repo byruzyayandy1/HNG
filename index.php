4131df342f81a32c84202dcdeaf4dda2d0d5a3e7
tracie96 <tracyamara07@gmail.com>
2018-04-15 15:29:23 +0000

    added

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>hng</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<header>
+	<h2>TracySoft <br> HNG ROCKS</h2>
+	<div class="line"><hr><br><hr><br><hr></div78 >
+</header>
+<div class="content">
+	<h1 class="hng">HNG INTERNSHIP 4.0</h1>
+	<div class="block">
+<p><center><font color="#000">We Rock!!!<br></font></center>
+<br><CENTER><?php 
+echo "The time is ".date("h:i:a");?></CENTER>
+</p>
+
+</div>
+</div>
+</body>
+</html>
